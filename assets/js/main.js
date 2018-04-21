ba19e736220e893c0a5eafebac401865ddf6b921
Cornelius Bezuidenhout <kerneels.bez@gmail.com>
2018-04-21 20:51:57 +0200

    Added copy functionality to table cells

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,32 @@ function createWarning( warningContent ) {
   warningElement.innerHTML = '<b>Warning - </b> ' + warningContent;  
   return warningElement;
 }
+
+document.addEventListener( 'click', ( event ) => {
+  if( event.target.classList.contains('cp') ) {
+    const tempTextArea = document.createElement('textArea');
+    tempTextArea.value = event.target.innerText;
+    const valueCopied = tempTextArea.value;
+    document.body.appendChild( tempTextArea );
+    tempTextArea.select();
+
+    try{
+      let copySuccessfull = document.execCommand('copy');
+
+      if( copySuccessfull ) {
+        event.target.innerText = 'Copied';
+      } else {
+        event.target.innerText = 'Could not copy';
+      }
+    } catch ( error ) {
+      console.error(error);
+    }
+
+    document.body.removeChild(tempTextArea);
+    setTimeout( resetElement, 1000, event.target, valueCopied);
+  }
+});
+
+function resetElement( element, content ) {
+  element.innerText = content;
+}
