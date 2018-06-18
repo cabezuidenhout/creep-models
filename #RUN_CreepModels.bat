6f23f79898545a691f5fb63834594feb60558654
Cornelius Bezuidenhout <kerneels.bez@gmail.com>
2018-06-18 21:58:17 +0200

    Starting with data only

--- #RUN_CreepModels.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Creep Models Launcher
-set currentpath=%cd%
-set octavepath=%cd%\octave\bin\octave-cli-4.2.1.exe
-set scriptpath=%currentpath%\CreepModels.m
-start "Creep Models" %octavepath% %scriptpath%
